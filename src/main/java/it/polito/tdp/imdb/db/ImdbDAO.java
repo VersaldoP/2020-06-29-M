e43096810cd502cbe3bd7b1071e35e1e540a50ed
VersaldoP <79835869+VersaldoP@users.noreply.github.com>
2021-06-27 11:34:49 +0200

    sol 1.b

--- src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
+++ src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import it.polito.tdp.imdb.model.Actor;
 import it.polito.tdp.imdb.model.Director;
 import it.polito.tdp.imdb.model.Movie;
@@ -61,19 +64,24 @@ public class ImdbDAO {
 	}
 	
 	
-	public List<Director> listAllDirectors(){
-		String sql = "SELECT * FROM directors";
-		List<Director> result = new ArrayList<Director>();
+	public Map<Integer, Director> listAllDirectors(int anno){
+		String sql = "SELECT distinct d.id AS id ,d.first_name AS fn, d.last_name AS ln "
+				+ "from directors d,movies m,movies_directors md "
+				+ "WHERE d.id=md.director_id "
+				+ "AND m.id=md.movie_id "
+				+ "AND m.year =?";
+		Map<Integer,Director> result = new HashMap<>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, anno);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
 
-				Director director = new Director(res.getInt("id"), res.getString("first_name"), res.getString("last_name"));
+				Director director = new Director(res.getInt("id"), res.getString("fn"), res.getString("ln"));
 				
-				result.add(director);
+				result.put(res.getInt("id"),director);
 			}
 			conn.close();
 			return result;
--- src/main/java/it/polito/tdp/imdb/model/Model.java
+++ src/main/java/it/polito/tdp/imdb/model/Model.java
@@ -1,5 +1,38 @@
 package it.polito.tdp.imdb.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.imdb.db.ImdbDAO;
+
 public class Model {
+	
+	private ImdbDAO dao;
+	private SimpleWeightedGraph<Director,DefaultWeightedEdge> grafo;
+	private Map<Integer,Director> idMap;
+	private List<Adiacenze> archi;
+	
+	public Model() {
+		dao = new ImdbDAO();
+		
+	}
+	
+	public void creaGrafo(int anno) {
+		
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		idMap= new HashMap<>(dao.listAllDirectors(anno));
+		Graphs.addAllVertices(this.grafo, idMap.values());
+		
+		
+		
+	}
+	
+	
+	
 
 }
